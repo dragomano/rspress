82e3006ceb976a58420703ee95d1024d1889006d
Bugo <229402+dragomano@users.noreply.github.com>
2025-11-26 19:17:01 +0500

    Update ru/guide/use-mdx/_escape-hatch.tsx

--- website/docs/ru/guide/use-mdx/_escape-hatch.tsx
+++ website/docs/ru/guide/use-mdx/_escape-hatch.tsx
@@ -1,7 +1,7 @@
 export default () => {
   return (
     <p className="rp-doc">
-      Это содержимое в tsx, но стили такие же, как в документации, например{' '}
+      Это содержимое в tsx, но стили такие же, как в документации, например,
       <code>@rspress/core</code>. Однако этот текст с className="rp-not-doc"
       <code className="rp-not-doc">@rspress/core</code> не будет стилизоваться
     </p>
